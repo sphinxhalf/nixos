84371e29cd0606b68580b678059bd916952f9e1b
nghia <nghiavu7636@gmail.com>
2025-11-29 15:12:49 +0700

    add desktop config

--- flake.nix
+++ flake.nix
@@ -25,6 +25,20 @@
           }
         ];
       };
+
+      dell-precision-t3600 = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./hosts/dell-precision-t3600/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.nghia = import ./home.nix;
+
+          }
+        ];
+      };
     };
   };
 }
